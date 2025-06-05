b7a49f50080b028865d4878ce6f1a7b20f2640f1
NguyenThangUIT <127591983+NguyenThangUIT@users.noreply.github.com>
2025-06-05 16:11:44 +0700

    Delete colleration.py

--- colleration.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-def calculate_correlation(df: pd.DataFrame, attr1: str, attr2: str) -> float:
-    """Tính hệ số tương quan Pearson giữa hai thuộc tính."""
-    return df[attr1].corr(df[attr2])
